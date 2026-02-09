18cc77ee2daf1b82e5b4cf6586f4f1fd9b474bfa
kkupschi <kkupschi@gmail.com>
2026-02-09 13:55:08 +0100

    bootstrap AppComponent in main.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('portfolio-clean');
-}
+export class AppComponent {}
---
+++ src/app/sections/why-me/why-me.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-why-me',
+  imports: [],
+  templateUrl: './why-me.html',
+  styleUrl: './why-me.scss',
+})
+export class WhyMe {
+
+}
